8e43e41a5008f3d1fbe0720db91e4045e6423b50
Steven7sun <91993699+Steven7sun@users.noreply.github.com>
2021-12-09 10:15:38 -0800

    Add files via upload

---
+++ GroundSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GroundSpawner: MonoBehaviour
+{
+	public GameObject groundTile;
+	Vector3 nextSpawnPoint;
+    // Start is called before the first frame update
+	void SpawnTile(){
+	GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
+	nextSpawnPoint = temp.transform.GetChild(1).transform.position;
+}
+   private void Start()
+    {
+	SpawnTile();
+	SpawnTile();
+        
+    }
+}
